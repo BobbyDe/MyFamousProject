79a07f42c2040b92a8c1b22212f08b84da85dbe8
Boisney Philippe <phil.boisney@gmail.com>
2018-03-08 20:38:49 +0100

    First Commit

---
+++ MyNews/app/src/main/java/com/zafindratafa/terence/mynews/Controllers/Activities/MainActivity.java
@@ -0,0 +1,43 @@
+package com.zafindratafa.terence.mynews.Controllers.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import com.zafindratafa.terence.mynews.R;
+
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_main_toolbar)
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Configure Toolbar
+        this.configureToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu and add it to the Toolbar
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+    // ---------------
+    // CONFIGURATION
+    // ---------------
+
+    private void configureToolbar(){
+        // Get the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        // Sets the Toolbar
+        setSupportActionBar(toolbar);
+    }
+}
